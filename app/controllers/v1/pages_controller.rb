3889339d2382e25e47136b443e9cc75c742e0034
Brian <riceb53@gmail.com>
2018-04-29 10:46:45 -0500

    make some arbitrary changes

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,5 +1,7 @@
 class V1::PagesController < ApplicationController
   def index
+
+
     render json: {message: 'hello', message_new: 'Imron', name: 'becca', message_another: 'sean', message_2: 'Ben Sofer', message_3: 'Brian Rice'}
   end
 end
