a83457382222c1b8929e5084b459dbe99293a3fa
Joe Trask <YOUR@EMAIL.com>
2018-04-29 11:19:36 -0500

    added my message

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,18 +1,5 @@
 class V1::PagesController < ApplicationController
   def index
-<<<<<<< HEAD
-<<<<<<< HEAD
-    render json: {message: 'hello', message2: 'Hey, this is Joe'}
-=======
-<<<<<<< HEAD
-
-    render json: {message: 'hello', message_new: 'Imron', name: 'becca', message_another: 'sean'}
-
-    render json: {message: 'hello', }
-
->>>>>>> d7cd9b856ad7704dd4a974aa9415ae8be31ae876
-=======
-    render json: {message: 'hello', message_new: 'Imron', name: 'becca', message_another: 'sean', message_2: 'Ben Sofer', message_3: 'Brian Rice', message_4: 'Jen'}
->>>>>>> 426a0c825a216e964353ac8161c99f08e7635c1b
+    render json: {message: 'hello', message_new: 'Imron', name: 'becca', message_another: 'sean', message_2: 'Ben Sofer', message_3: 'Brian Rice', message_4: 'Jen', message_5: 'Joe'}
   end
 end
