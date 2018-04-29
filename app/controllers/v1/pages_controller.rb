c46b3448b1ac8c95f48ac64715ce38520ec2462a
Brian <riceb53@gmail.com>
2018-04-29 10:37:26 -0500

    add my name

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,6 +1,7 @@
 class V1::PagesController < ApplicationController
   def index
     render json: {message_1: 'hello',
-      message_2: 'Ben Sofer'
+      message_2: 'Ben Sofer',
+      message_3: 'Brian Rice'
     }
 end
