50c23e7f50f835cf65ef9193228bfa9dc73cb402
Ben_Sofer <bysofer@gmail.com>
2018-04-29 11:21:08 -0400

    added my name to the jason

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,5 +1,8 @@
 class V1::PagesController < ApplicationController
   def index
-    render json: {message: 'hello'}
+    render json: {
+      message: 'hello',
+      message: 'Ben Sofer'
+    }
   end
 end
