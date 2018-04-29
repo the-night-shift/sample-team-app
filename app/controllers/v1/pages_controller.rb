ff3f6e07400fb3621fa954d29d80f404be5b7419
Joe Trask <YOUR@EMAIL.com>
2018-04-29 10:33:36 -0500

    try again

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,5 +1,5 @@
 class V1::PagesController < ApplicationController
   def index
-    render json: {message: 'hello', message: 'Hey, this is Joe'}
+    render json: {message: 'hello', message2: 'Hey, this is Joe'}
   end
 end
