bb80848c8e3b298c155248099d7a03c057c789ab
Imron Khatib <imronskhatib@gmail.com>
2018-04-29 10:32:08 -0500

    Fix render issue

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,9 +1,9 @@
 class V1::PagesController < ApplicationController
   def index
 
-    render json: {message: 'hello', message_new: 'Imron', name: 'becca'}
+    render json: {message: 'hello', message_new: 'Imron', name: 'becca', message_another: 'sean'}
 
-    render json: {message: 'hello', message: 'sean'}
+    render json: {message: 'hello', }
 
   end
 end
