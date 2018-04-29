38270eef3438a8be1a7b3edc2aa54e16546e8df0
Barry <bdc1130@yahoo.com>
2018-04-29 11:21:44 -0500

    add contorller name

---
+++ app/controllers/barrys_controller.rb
@@ -0,0 +1,5 @@
+class BarrysController < ApplicationController
+    def
+       render json: {message: "Barry"} 
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'v1/Barry'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
     get '/pages' => 'pages#index'
