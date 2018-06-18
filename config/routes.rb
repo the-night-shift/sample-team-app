fcb3ce8b7b6fada3e6c6a2678f840a1122bc64c6
Brian <riceb53@gmail.com>
2018-06-18 10:16:32 -0500

    modify routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :v1 do
     get '/pages' => 'pages#index'
     get '/brians' => 'brians#index'
-
     get '/imrons' => 'imrons#index'
 
     get '/beccas' => 'beccas#index'
